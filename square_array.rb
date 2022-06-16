86b021556c69eff064cbd9d95f1ad553e04ad285
Aaron <a.lig.tucc@gmail.com>
2022-06-16 11:53:57 -0700

    commit tree 08a192ebe06666bf0563e01fa81f3d9e3e8c340c

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
-  arr = []
-  array.each do |number|
-    arr << number ** 2
+  sq_array = array.collect do |number|
+    number ** 2
   end
-  return arr
+  return sq_array
 end
